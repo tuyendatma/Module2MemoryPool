28217f15c5f00615c7d1688febdd50d4bc81aed5
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-05-28 08:27:19 +0700

    28/5/2020

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Gun gun=new Gun();
+        System.out.println("Start");
+//        gun.fire();
+        gun.fireInPool();
+        System.out.println("Game over");
+        System.out.println("Tocal bullet created: " + Bullet.count);
+    }
+}
